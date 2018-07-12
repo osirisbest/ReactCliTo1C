0ee11d5ac980f50384ebb45d5a4a27016805780e
osirisbest <osirisbest@gmail.com>
2018-07-12 22:32:55 +0300

    worked!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 import Client from '../node_modules/another-rest-client';
 
 const username = 'Name', passw = 'Name'
-const x = new XMLHttpRequest();
+//const x = new XMLHttpRequest();
 
 class App extends Component {
   constructor(props) {
@@ -16,6 +16,7 @@ class App extends Component {
     this.onClickClean = this.onClickClean.bind(this)
     this.onClickSimplest = this.onClickSimplest.bind(this)
     this.onClickReqPost = this.onClickReqPost.bind(this)
+    this.onClickCreateDocumentRequest=this.onClickCreateDocumentRequest.bind(this)
     this.state = { log: 'log' }
   }
   render() {
@@ -27,10 +28,17 @@ class App extends Component {
         <p className="App-intro">
         </p>
         <ButtonGroup vertical>
+        <form className="commentForm">
+        <input type="text" placeholder="Your name" />
+        <input type="text" placeholder="Say something..." />
+        <input type="submit" value="Post" />      
+      </form>
           <Button bsStyle='primary' onClick={this.onClick} >Тестовый onClick запрос выполнить</Button>
           <Button bsStyle='success' onClick={this.onClickClean} >XMLHttpRequest onClickClean запрос с авторизацией</Button>
           <Button bsStyle='success' onClick={this.onClickSimplest} >XMLHttpRequest onClickSimplest Простейший рабочий запрос к локалхосту</Button>
           <Button bsStyle='success' onClick={this.onClickReqPost} >XMLHttpRequest onClickReqPost создание справочника</Button>
+          <Button bsStyle='success' onClick={this.onClickCreateDocumentRequest} >XMLHttpRequest onClickCreateDocumentRequest создание документа</Button>
+          
         </ButtonGroup>
         <h2>{this.state.log}</h2>
       </div>
@@ -49,20 +57,11 @@ class App extends Component {
       console.log(response)
     })
   }
-
-  onClickReqPost() {
-    x.open("POST", "http://localhost/SC/odata/standard.odata/Catalog_sampleref?$format=json", true);
-    x.withCredentials = true;
-    x.onload = () => {
-      console.log(x.responseText);
-      this.setState({ log: x.responseText })
-    }
-    x.send(JSON.stringify({ "Description": "Test" }));
-  }
-
+ 
   onClickClean() {
     let x = new XMLHttpRequest();
     x.open("GET", "http://localhost/SC/odata/standard.odata", true);
+    x.setRequestHeader('Access-Control-Allow-Origin','http://localhost:3000')
     x.withCredentials = true;
     x.onload = () => {
       this.setState({ log: x.responseText })
@@ -71,13 +70,66 @@ class App extends Component {
   }
 
   onClickSimplest() {
-    const x = new XMLHttpRequest();
+    let x = new XMLHttpRequest();
     x.open("GET", "http://localhost/", true);
     x.onload = () => {
       this.setState({ log: x.responseText })
     }
+    x.onerror = () => {
+      console.log(x.status)
+      console.log(x.statusText)
+      console.log(x.responseText);
+      this.setState({ log: x.responseText })
+    }
     x.send(null);
   }
+
+  onClickReqPost() {
+    let x = new XMLHttpRequest();
+    x.withCredentials = true;
+    x.open("POST", "http://localhost/SC/odata/standard.odata/Catalog_sampleref?$format=json", true);
+    //x.setRequestHeader('Access-Control-Allow-Origin','http://localhost:3000')
+    
+    x.onload = () => {
+      console.log(x.responseText);
+      this.setState({ log: x.responseText })
+    }
+    x.onerror = () => {
+      console.log(x.status)
+      console.log(x.statusText)
+      console.log(x.responseText);
+      this.setState({ log: x.responseText })
+    }
+    x.send(JSON.stringify({ "Description": "Test" }));
+  }
+
+  onClickCreateDocumentRequest() {
+    let x = new XMLHttpRequest();
+    x.withCredentials = true;
+    x.open("POST", "http://localhost/SC/odata/standard.odata/Document_СЦентр_Обращение?$format=json", true);
+    
+    //x.mozBackgroundRequest = true;
+    
+   // x.setRequestHeader('Content-type','application/json');
+    //x.setRequestHeader('Access-Control-Allow-Origin','http://localhost:3000')
+    //x.setRequestHeader('Authorization','Basic TmFtZTpOYW1l')
+    //x.setRequestHeader('Authorization', 'Basic ' + Buffer.from(username + ':' + passw).toString('base64'))
+    
+    x.onload = () => {
+      console.log(x.status)
+      console.log(x.statusText)
+      console.log(x.responseText);
+      this.setState({ log: x.responseText })
+    }
+    x.onerror = () => {
+      console.log(x.status)
+      console.log(x.statusText)
+      console.log(x.responseText);
+      this.setState({ log: x.status })
+    }
+    x.send(JSON.stringify({"ОписаниеНеисправности": "Тестовое описание. Все поломалось,все плохо_","Контрагент_Key": "4f2cfe01-7f9d-11e8-8079-d46e0e0c6a39"}));
+  }
 }
+
 
 export default App;
