03339a5912294b94012f6ac7582b66c1341b9294
osirisbest <osirisbest@gmail.com>
2018-07-14 10:39:12 +0300

    prod

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import logo from './omnic.png'
 import './App.css'
 //import Client from '../node_modules/another-rest-client';
-
-const addr = 'http://192.168.200.139'
+const dev=false
+const addr = 'http://192.168.200.139/sc/'
 
 class App extends Component {
   constructor(props) {
@@ -14,9 +14,10 @@ class App extends Component {
     this.onClickCreateDocumentRequest = this.onClickCreateDocumentRequest.bind(this)
     this.onChangeError = this.onChangeError.bind(this)
     this.onChangeComment = this.onChangeComment.bind(this)
-    this.state = { log: 'log' }
+    this.onClКонтрагент_Key=this.onClКонтрагент_Key.bind(this)
+    this.state = { log: '' ,Контрагент_Key:"4f2cfe01-7f9d-11e8-8079-d46e0e0c6a39"}
   }
-  render() {
+ render() {
     return (
       <div className="App">
         <header className="App-header">
@@ -25,6 +26,7 @@ class App extends Component {
         <p className="App-intro">
         </p>
         <h1>Опишите свою проблему и нажмите клавишу 'Создать обращение' для отправки в сервисный центр</h1>
+        <h3>Для создания обращения поле `описание неисправности` должно быть уникально</h3>
         <form className="commentForm">
           <textarea type="text" placeholder="Комментарий" cols="78" rows="5" onChange={this.onChangeComment} />
         </form>
@@ -34,39 +36,64 @@ class App extends Component {
         <ButtonGroup vertical>
           <Button bsStyle='success' onClick={this.onClickCreateDocumentRequest} >Создать обращение</Button>
         </ButtonGroup>
-        <h2>{this.state.log}</h2>
+        <h3>Ответ сервера информационной системы сервисного центра:</h3>
+        <form>
+        <textarea value={this.state.log} cols="78" rows="5"></textarea>
+        </form>
+        <label>Контрагент_Key:</label><input type='text' onChange={this.onClКонтрагент_Key} size='60' value={this.state.Контрагент_Key} id='Контрагент_Key'/>
+         
       </div>
-    );
+    )
   }
-
+  componentDidMount(){
+    let Контрагент_Key=localStorage.getItem('Контрагент_Key')
+    if (Контрагент_Key) {
+      this.setState({ Контрагент_Key: Контрагент_Key})
+    }
+  }
+  onClКонтрагент_Key(event){
+    this.setState({ Контрагент_Key: event.target.value })
+    localStorage.setItem('Контрагент_Key',event.target.value)
+  }
   onChangeError(event) {
     this.setState({ error: event.target.value })
   }
-
   onChangeComment(event) {
-    this.setState({ error: event.target.comment })
+    this.setState({ comment: event.target.value })
   }
-
   onClickCreateDocumentRequest() {
-    let x = new XMLHttpRequest();
+    let x = new XMLHttpRequest()
     x.withCredentials = true;
 
-    x.open("POST", addr + '/sc/odata/standard.odata/Document_СЦентр_Обращение?$format=json', true);
+    x.open("POST", addr + 'odata/standard.odata/Document_СЦентр_Обращение?$format=json', true)
 
-    x.onload = () => {
+    x.onload = () => { if (dev){
       console.log('status:' + x.status)
       console.log('statusText:' + x.statusText)
-      console.log('responseText:' + x.responseText);
-      this.setState({ log: x.responseText })
+      console.log('responseText:' + x.responseText)}
+      // eslint-disable-next-line
+      let messageValue=''
+      // eslint-disable-next-line
+      let resInJSON
+      try{
+      resInJSON=JSON.parse(x.responseText)
+      //eslint-disable-next-line
+      messageValue=resInJSON['odata.error'].message.value}
+      catch(err){
+        messageValue='Создан документ "Обращение" № '+resInJSON.Number +' от '+resInJSON.Date+'\nRef_Key:'+resInJSON.Ref_Key+'\n'
+        alert('Инженеры службы сервиса уведомлены о Вашей проблеме и приступят к ее устранению \n'+messageValue)
+      }
+      this.setState({ log: messageValue })
     }
-    x.onerror = () => {
+    x.onerror = () => { if (dev){
       console.log('status:' + x.status)
       console.log('statusText:' + x.statusText)
-      console.log('responseText:' + x.responseText);
-      this.setState({ log: x.status })
+      console.log('responseText:' + x.responseText)}
+      this.setState({ log: x.responseText +"\n"})
     }
-
-    x.send(JSON.stringify({ "ОписаниеНеисправности": this.state.error, "Контрагент_Key": "4f2cfe01-7f9d-11e8-8079-d46e0e0c6a39", "Комментарий": this.state.comment }));
+    //"4f2cfe01-7f9d-11e8-8079-d46e0e0c6a39"
+    
+    x.send(JSON.stringify({ "ОписаниеНеисправности": this.state.error, "Контрагент_Key":this.state.Контрагент_Key, "Комментарий": this.state.comment }))
   }
 }
 
